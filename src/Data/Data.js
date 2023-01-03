67040b37f5d0055650d383e98acb6a14b1416366
lp700ss <siddhantsaikia@gmail.com>
2023-01-03 16:18:16 +0530

    sidebar

---
+++ src/Data/Data.js
@@ -0,0 +1,35 @@
+import {
+    UilEstate,
+    UilClipboardAlt,
+    UilUsersAlt,
+    UilPackage,
+    UilChart,
+    UilSignOutAlt,
+  } from "@iconscout/react-unicons";
+  
+  // Analytics Cards imports
+  import { UilUsdSquare, UilMoneyWithdrawal } from "@iconscout/react-unicons";
+  import { keyboard } from "@testing-library/user-event/dist/keyboard";
+
+  export const SidebarData = [
+    {
+        icon: UilEstate,
+        heading: "Dasboard",
+    },
+    {
+        icon: UilClipboardAlt,
+        heading: "Orders",
+    },
+    {
+        icon: UilUsersAlt,
+        heading: "Customers",
+    },
+    {
+        icon: UilPackage,
+        heading: "Products",
+    },
+    {
+        icon: UilChart,
+        heading: "Analytics"
+    },
+  ];
